ef29b79f143c623121c5e5393e9267589383fbab
JTEnglish <je787413@ohio.edu>
2019-04-09 14:04:51 -0400

    simple text color class for terminal output

---
+++ src/TerminalColors.py
@@ -0,0 +1,14 @@
+'''
+ Class: TerminalColors
+ Credit: https://stackoverflow.com/questions/287871/print-in-terminal-with-colors
+'''
+
+class TerminalColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
