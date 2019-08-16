a49cb713cd9ee47e203f6717a742a9a6cd048121
JTEnglish <je787413@ohio.edu>
2019-08-16 15:29:41 -0400

    replace old example file

--- src/example.py
+++ src/example.py
@@ -7,40 +7,51 @@
 # Example usage for UAVHeading Class
 ############################################
 
+import matplotlib.pyplot as plt
 from UAVHeading import UAVHeading
 
 def main():
                     # (pos, waypt, speed, heading, tPossible)
-    uav0 = UAVHeading((1.515593587271553, -132.53722833033987),
-                      (1.5156, -132.5111),
-                      50.0,
-                      90.0,
-                      30.0)
-
-    uav1 = UAVHeading((1.5098039166143598, -132.531099),
-                      (1.5267, -132.531),
+    uav0 = UAVHeading((7, 0),
+                      (20, 0),
                       50.0,
                       0.0,
                       30.0)
 
-    waypoints, _ = uav0.avoid(uav1)
-    print(waypoints)
-
-                    # (pos, waypt, speed, heading, tPossible)
-    uav0 = UAVHeading((1.5156, -132.5423),
-                      (1.5156, -132.5111),
+    uav1 = UAVHeading((10, -3),
+                      (1.5267, -132.531),
                       50.0,
                       90.0,
                       30.0)
-
-    uav1 = UAVHeading((1.5156, -132.5321),
-                      (1.5156, -132.403),
+    
+    uav2 = UAVHeading((13, 0),
+                      (1.5267, -132.531),
                       50.0,
-                      270.0,
+                      180.0,
                       30.0)
 
-    waypoints, _ = uav0.avoid(uav1)
-    print(waypoints)
+    koz0 = [(15,0), (19,2), (20,1), (16,-1), (15,0)]
+
+    uav_others = [uav1, uav2]
+    static_kozs = [koz0]
+
+    # plot uav0 position and goal
+    plt.plot(uav0.position[0], uav0.position[1], 'oc', label='UAV0 Position')
+    plt.plot(uav0.waypoint[0], uav0.waypoint[1], 'xc', label='UAV0 Goal')
+
+    # plot uav and koz positions
+    for uav in uav_others:
+        plt.plot(uav.position[0], uav.position[1], 'or', label='Other UAV')
+    for koz in static_kozs:
+        x = [pt[0] for pt in koz]
+        y = [pt[1] for pt in koz]
+        plt.plot(x, y, '--r', label='Keep Out Zone')
+    plt.grid(True)
+    plt.legend()
+    plt.axis('equal')
+    plt.show()
+
+    uav0.avoid(uav_others, static_kozs)
 
 if __name__ == '__main__':
     main()
